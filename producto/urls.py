f5bebc3cad78c86a66ec1eb7532f38581b4204c7
Alex Hector Astuhuaman Delgado <alhexanderad@gmail.com>
2022-03-20 18:59:30 -0500

    Se realizo el pdf de  lista de productos para poder dar estilos es necesario realizar de manera independiente

--- producto/urls.py
+++ producto/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
-from producto.views import ListaProducto
+from producto.views import ListaProducto, pdf_report
 
 app_name='producto'
 
 
 urlpatterns = [
   path('lista/', ListaProducto.as_view() ,name="ListaProsucto"),
+  path('pdf/', pdf_report , name="PDF-Producto"),
+  #path('pdf/', ProductPdfView , name="PDF-Producto"),
 ]
 
 
--- producto/views.py
+++ producto/views.py
@@ -5,8 +5,37 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from producto.models import Product
 
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+# from django.views.generic.detail import DetailView
+# from django_xhtml2pdf.views import PdfMixin
 class ListaProducto(ListView):
   model = Product
   context_object_name = 'productos'
   template_name = 'producto/lista.html'
   
+def pdf_report(request):
+  productos = Product.objects.all()
+  print(productos)
+  context = {'productos': productos}
+  print("asdfasñldmf:",context)
+  template_path = 'producto/reportepdf.html'
+  # Create a Django response object, and specify content_type as pdf
+  response = HttpResponse(content_type='application/pdf')
+  response['Content-Disposition'] = 'attachment; filename="productos.pdf"'
+  # find the template and render it.
+  template = get_template(template_path)
+  html = template.render(context)
+
+  # create a pdf
+  pisa_status = pisa.CreatePDF(html, dest=response)
+  # if error then show some funy view
+  if pisa_status.err:
+      return HttpResponse('We had some errors <pre>' + html + '</pre>')
+  return response
+
+# class ProductPdfView(PdfMixin):
+#     model = Product
+#     context_object_name = 'productos'
+#     template_name = 'producto/reportepdf.html'
